15a3f05f40f9e45d8506ca41948d4fb5c0798ed4
JorgetheDev <39925365+JorgetheDev@users.noreply.github.com>
2020-04-27 20:07:27 -0500

    Added Db script

---
+++ sql/spring 2020 sql script.sql
@@ -0,0 +1,4 @@
+select * from applicant;
+
+insert into applicant (id, first_name, last_name, address, city, license_type, driver_license_matched, prof_license_matched)
+Values (1, "bob", "smith", "123 valley lane", "tx", 1, 1, 1);
